c0682abb1e332bffd71333931e7922f75e0e5c55
Abhishek TK <59571289+maa-atk@users.noreply.github.com>
2023-11-11 10:47:21 +0530

    upload-site

--- script.js
+++ script.js
@@ -1,26 +1,115 @@
+let slider = document.getElementById("slider");
+const faceLabel = document.getElementById("face-label");
+const body = document.body;
 
-console.log('Its working')
-
-let theme = localStorage.getItem('theme')
-
-if (theme == null) {
-	setTheme('light')
+if (localStorage.getItem("preferredMode") === "dark") {
+  enableDarkMode();
+  slider.value = 1;
 } else {
-	setTheme(theme)
+  disableDarkMode();
 }
 
-let themeDots = document.getElementsByClassName('theme-dot')
+slider.addEventListener("input", () => {
+  if (slider.value === "1") {
+    enableDarkMode();
+  } else {
+    disableDarkMode();
+  }
+});
 
-
-for (var i = 0; themeDots.length > i; i++) {
-	themeDots[i].addEventListener('click', function () {
-		let mode = this.dataset.mode
-		console.log('Option clicked:', mode)
-		setTheme(mode)
-	})
+function checkColorMode(theme) {
+  if (theme === "dark") {
+    document.documentElement.style.setProperty("--right-card-frost", "#4F2B3D");
+    document.documentElement.style.setProperty("--left-card-frost", "#2F4F4F");
+    document.documentElement.style.setProperty("--font-color", "#e9cab0");
+  }
+  if (theme === "light") {
+    document.documentElement.style.setProperty(
+      "--right-card-frost",
+      "rgba(229, 172, 215, 0.56)"
+    );
+    document.documentElement.style.setProperty(
+      "--left-card-frost",
+      "rgba(215, 229, 172, 0.54)"
+    );
+    document.documentElement.style.setProperty("--font-color", "#044d9a");
+  }
 }
-const $ = s => document.querySelectorAll(s);
-const on = (ev, el, cb) =>
-	el.addEventListener(ev, e => cb(e, el));
-$('a').forEach((el) =>
-	on('click', el, e => e.preventDefault()));
+
+function toggleStarsVisibility(theme) {
+  const starsContainer = document.querySelector(".stars");
+  starsContainer.style.display = theme === "dark" ? "block" : "none";
+}
+
+function toggleCloudsVisibility(theme) {
+  const cloudContainer = document.querySelector(".cloud-container");
+  cloudContainer.style.display = theme === "light" ? "block" : "none";
+}
+
+function enableDarkMode() {
+  body.classList.add("dark-theme");
+  body.classList.remove("light-theme");
+  faceLabel.textContent = "🌙";
+  toggleStarsVisibility("dark");
+  toggleCloudsVisibility("dark");
+  checkColorMode("dark");
+  localStorage.setItem("preferredMode", "dark");
+}
+function disableDarkMode() {
+  body.classList.remove("dark-theme");
+  body.classList.add("light-theme");
+  faceLabel.textContent = "🌞";
+  toggleStarsVisibility("light");
+  toggleCloudsVisibility("light");
+  checkColorMode("light");
+  localStorage.setItem("preferredMode", "light");
+}
+
+// stars
+const darkThemeContainer = document.querySelector(".dark-theme .stars");
+darkThemeContainer.style.position = "fixed";
+darkThemeContainer.style.top = "0";
+darkThemeContainer.style.left = "0";
+
+const numDarkThemeStars = 100;
+
+for (let i = 0; i < numDarkThemeStars; i++) {
+  const star = document.createElement("div");
+  star.className = "star";
+
+  const starSize = Math.random() * 10 + 5;
+  star.style.width = starSize + "px";
+  star.style.height = starSize + "px";
+  star.style.backgroundColor = randomStarColor();
+  star.style.left = Math.random() * 100 + "vw";
+  star.style.top = Math.random() * 100 + "vh";
+  darkThemeContainer.appendChild(star);
+}
+
+function randomStarColor() {
+  const colors = ["#ffffff", "#ffffcc", "#ffcc99", "#ffcccc"];
+  const randomIndex = Math.floor(Math.random() * colors.length);
+  return colors[randomIndex];
+}
+
+// clouds
+const cloudContainer = document.querySelector(".cloud-container");
+cloudContainer.style.position = "fixed";
+cloudContainer.style.top = "0";
+cloudContainer.style.left = "0";
+
+const starPositions = Array.from(
+  darkThemeContainer.querySelectorAll(".star")
+).map((star) => {
+  return { left: star.style.left, top: star.style.top };
+});
+
+starPositions.forEach((position) => {
+  const cloud = document.createElement("div");
+  cloud.className = "cloud";
+  cloud.style.left = position.left;
+  cloud.style.top = position.top;
+  const randomCloudSize = `${Math.random() * 2 + 0.1}em`;
+  cloud.style.setProperty("--cloud-size", randomCloudSize);
+  cloudContainer.appendChild(cloud);
+});
